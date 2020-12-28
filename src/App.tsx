43c727e94d017f3d2238c498d5c0486bf329965a
sam <3161728@gmail.com>
2020-12-28 11:45:42 +0900

    Configurate graphql and apollo

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="min-h-full bg-red-500 h-full">
-      <h1 className="text-xl text-white">Hello Tailwind.</h1>
+    <div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
