b6f0cff9994c5f23cf4838f7dea6c29d33a9c1d1
sam <3161728@gmail.com>
2021-01-11 11:07:00 +0900

    Extending the Dish Component

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,12 +1,13 @@
-import { RESTAURANT_FRAGMENT } from "../../fragments";
+import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { gql, useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import React from "react";
 import {
   restaurant,
   restaurantVariables,
 } from "../../__generated__/restaurant";
-import { Helmet } from "react-helmet-async";
-import React from "react";
+import { Dish } from "../../components/dish";
 
 const RESTAURANT_QUERY = gql`
   query restaurant($input: RestaurantInput!) {
@@ -15,10 +16,14 @@ const RESTAURANT_QUERY = gql`
       error
       results {
         ...RestaurantParts
+        menu {
+          ...DishParts
+        }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
+  ${DISH_FRAGMENT}
 `;
 
 interface IRestaurantParams {
@@ -27,38 +32,45 @@ interface IRestaurantParams {
 
 export const Restaurant = () => {
   const { id } = useParams<IRestaurantParams>();
-  const { loading, data } = useQuery<restaurant, restaurantVariables>(
-    RESTAURANT_QUERY,
-    {
-      variables: { input: { restaurantId: +id } },
-    }
-  );
+  const { data } = useQuery<restaurant, restaurantVariables>(RESTAURANT_QUERY, {
+    variables: { input: { restaurantId: +id } },
+  });
 
   return (
     <div>
       <Helmet>
         <title>{data?.restaurant.results?.name || ""} | Nuber Eats</title>
       </Helmet>
-      {!loading && (
-        <div
-          className=" bg-gray-800 bg-center bg-cover py-48"
-          style={{
-            backgroundImage: `url(${data?.restaurant.results?.coverImg})`,
-          }}
-        >
-          <div className="bg-white w-3/12 py-8 pl-10 lg:pl-28">
-            <h4 className="text-4xl mb-3">{data?.restaurant.results?.name}</h4>
-            <Link to={`/category/${data?.restaurant.results?.category?.name}`}>
-              <h5 className="text-sm font-light mb-2">
-                {data?.restaurant.results?.category?.name}
-              </h5>
-            </Link>
-            <h6 className="text-sm font-light">
-              {data?.restaurant.results?.address}
-            </h6>
-          </div>
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.results?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-10 lg:pl-28">
+          <h4 className="text-4xl mb-3">{data?.restaurant.results?.name}</h4>
+          <Link to={`/category/${data?.restaurant.results?.category?.name}`}>
+            <h5 className="text-sm font-light mb-2">
+              {data?.restaurant.results?.category?.name}
+            </h5>
+          </Link>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.results?.address}
+          </h6>
         </div>
-      )}
+      </div>
+      <div className="container grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+        {data?.restaurant.results?.menu.map((dish, index) => (
+          <Dish
+            key={index}
+            name={dish.name}
+            description={dish.description}
+            price={dish.price}
+            isCustomer={true}
+            options={dish.options}
+          />
+        ))}
+      </div>
     </div>
   );
 };
