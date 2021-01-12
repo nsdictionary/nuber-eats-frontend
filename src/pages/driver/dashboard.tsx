780f8d580dfbf2659c4c49aad2faaac42085cf0a
sam <3161728@gmail.com>
2021-01-12 14:52:58 +0900

    Add driver dashboard page

---
+++ src/pages/driver/dashboard.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import GoogleMapReact from "google-map-react";
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
+
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚖</div>;
+
+export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<google.maps.Map>();
+  const [maps, setMaps] = useState<any>();
+  // @ts-ignore
+  const onSucces = ({ coords: { latitude, longitude } }: Position) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  // @ts-ignore
+  const onError = (error: PositionError) => {
+    console.log(error);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSucces, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      /* const geocoder = new google.maps.Geocoder();
+      geocoder.geocode(
+        {
+          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        },
+        (results, status) => {
+          console.log(status, results);
+        }
+      ); */
+    }
+  }, [driverCoords.lat, driverCoords.lng, map, maps]);
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
+  return (
+    <div>
+      <div
+        className="overflow-hidden"
+        style={{ width: window.innerWidth, height: "50vh" }}
+      >
+        <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={16}
+          draggable={false}
+          defaultCenter={{
+            lat: 36.58,
+            lng: 125.95,
+          }}
+          bootstrapURLKeys={{ key: "AIzaSyA7luXUiadLSv1oazhK__m5o-DcqPk03CU" }}
+        >
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
+};
